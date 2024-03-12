c7bdf737204e68e25ca034b706e6429e1f2e6f24
Shreya21121 <113017551+Shreya21121@users.noreply.github.com>
2024-03-12 20:35:43 +0530

    Add files via upload

---
+++ module 24/1.js
@@ -0,0 +1,4 @@
+console.log(addnos(5,10));
+function addnos(num1, num2){
+    return num1+num2;
+}
---
+++ module 24/2.js
@@ -0,0 +1,4 @@
+console.log(multiplynos(3,4));
+const multiplynos= function (num1,num2){
+    return num1* num2;
+};
